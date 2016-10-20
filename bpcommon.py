72bcd25455e989d19c9f1ede975af84e1ad07c1b
Michael Gagnon <mikegagnon@gmail.com>
2016-10-20 15:43:02 -0600

    Toying around with new_pet method

---
+++ bpcommon.py
@@ -0,0 +1,17 @@
+from string import Template
+import flask
+
+# TODO
+
+ERROR_MISSING_JSON_INPUT = 1
+
+def error_missing_json_input(url):
+    template = Template("Error: You may only access $url by POSTing JSON.")
+    message = template.substitute(url=url)
+    return error_json(ERROR_MISSING_JSON_INPUT, message)
+
+def error_json(code, message):
+    return flas.jsonify({
+            "error-code": code,
+            "message": message
+        })
--- management.py
+++ management.py
@@ -1,10 +1,79 @@
+import sqlite3
+import bpcommon
+import flask
+from jsonschema import validate, ValidationError
 from flask import Flask, request
 management = Flask(__name__)
 
+DATABASE = None
+
+# TODO: put in another file?
+NEW_PET_REQUEST_SCHEMA = {
+    "type": "object",
+    "properties": {
+        "name": {
+            "type": "string"
+        },
+        "strength": {
+            "type": "number",
+            "minimum": 0,
+            "maximum": 1
+        },
+        "agility": {
+            "type": "number",
+            "minimum": 0,
+            "maximum": 1
+        },
+        "wit": {
+            "type": "number",
+            "minimum": 0,
+            "maximum": 1
+        },
+        "senses": {
+            "type": "number",
+            "minimum": 0,
+            "maximum": 1
+        }
+    },
+    "required": ["name", "strength", "agility", "wit", "senses"]
+}
+
+
+
+
+
+# from http://flask.pocoo.org/docs/0.11/patterns/sqlite3/
+def get_db():
+    db = getattr(flask.g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+# from http://flask.pocoo.org/docs/0.11/patterns/sqlite3/
+@management.teardown_appcontext
+def close_connection(exception):
+    db = getattr(flask.g, '_database', None)
+    if db is not None:
+        db.close()
+
 @management.route("/new-pet", methods=["POST"])
 def new_pet():
+
+    ## TODO: Return errors (eg 400) as JSON?
     request_data = request.get_json()
+
+    if request_data == None:
+        request.on_json_loading_failed("No JSON object could be decoded")
+
+    try:
+        validate(request_data, NEW_PET_REQUEST_SCHEMA)
+    except ValidationError:
+        request.on_json_loading_failed("Your JSON post was valid JSON, " +
+            "however it does not match NEW_PET_REQUEST_SCHEMA")
+
     return str(request_data)
 
+# TODO: arguments from command line
 if __name__ == "__main__":
+    DATABASE = "test.db"
     management.run()
