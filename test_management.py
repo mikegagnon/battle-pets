c15b642b43ade08b2e17ca0b518e2138c45e0d0c
Michael Gagnon <mikegagnon@gmail.com>
2016-10-21 17:07:38 -0600

    tests for new_pet

--- test_management.py
+++ test_management.py
@@ -22,7 +22,7 @@ class ManagementTestCase(unittest.TestCase):
         os.unlink(management.app.config['DATABASE'])
 
     # TODO: reduce code duplication
-    def test_new_pet(self):
+    def test_new_pet_duplicate(self):
 
         request_data = json.dumps(
             {
@@ -48,6 +48,35 @@ class ManagementTestCase(unittest.TestCase):
         assert json.loads(rv.data) == {
                 "message": "A pet with the name 'foo' already exists."
             }
+
+    def test_new_pet_no_json(self):
+
+        request_data = "abc"
+
+        rv = self.app.post('/new-pet', data=request_data,
+            content_type='application/json')
+
+        assert rv.status == "400 BAD REQUEST"
+        assert json.loads(rv.data) == {
+                "message": "No JSON object could be decoded"
+            }
+
+    def test_new_pet_bad_schema(self):
+
+        request_data = json.dumps(
+            {
+              "name": "foo"
+            })
+
+        # This succeeds because foo is a new pet
+        rv = self.app.post('/new-pet', data=request_data,
+            content_type='application/json')
+
+        assert rv.status == "400 BAD REQUEST"
+        assert json.loads(rv.data)["message"] == \
+            "Your JSON post does not match NEW_PET_REQUEST_SCHEMA."
+
+
 
 if __name__ == '__main__':
     unittest.main()
