5ac58d26bab86bc7a4281b7d8f170ab609936613
Michael Gagnon <mikegagnon@gmail.com>
2016-10-23 15:56:36 -0600

    Finished test.sh

--- test.sh
+++ test.sh
@@ -24,6 +24,26 @@ python db.py
 # Fails because Charmander already exists
 ./new_pet.py --name Charmander --strength 0.5 --agility 0.0 --wit 0.3 --senses 0.2 --expect_400
 
+# Succeeds
 ./get_pet.py --name Dwebble 
 
-./get_pet.py --name Failure --expect_404 
+# Fails because there is no pet named Failure
+./get_pet.py --name Failure --expect_404
+
+# Fails because BadCategory does not match the schema
+./contest.py --name1 Charmander --name2 Blissey --category BadCategory --expect_400
+
+# Fails because Failure is not a valid pet name
+./contest.py --name1 Charmander --name2 Failure --category strength --expect_400
+
+# Fails because a pet cannot fight itself
+./contest.py --name1 Charmander --name2 Charmander --category strength --expect_400
+
+# Charmander wins because Charmander is stronger
+# This also tests arena-result because contest.py gets arena-result
+./contest.py --name1 Charmander --name2 Blissey --category strength
+
+# Blissey wins because Blissey has more wit
+./contest.py --name1 Charmander --name2 Blissey --category wit
+
+./history.py
