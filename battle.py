3ce34c5c7f6fe1c77da2f13acf5810f3281d8055
Michael Gagnon <mikegagnon@gmail.com>
2016-10-22 19:26:18 -0600

    New battle algorithm

--- battle.py
+++ battle.py
@@ -1,32 +1,41 @@
 import time
 
+# TODO: document
 # TODO: does battle or contest update the db?
 
-def petify(pet):
-    return {
-        "name": pet[0],
-        "strength": pet[1],
-        "agility": pet[2],
-        "wit": pet[3],
-        "senses": pet[4]
-    }
+class BattlePet():
 
-def do_battle(pets, category):
-    pet1 = petify(pets[0])
-    pet2 = petify(pets[1])
+    def __init__(self, pet_record):
+        self.name = pet_record[0]
+
+        self.attributes = {
+            "strength": pet_record[1],
+            "agility": pet_record[2],
+            "wit": pet_record[3],
+            "senses": pet_record[4],
+        }
+
+        self.wins = pet_record[5]
+        self.losses = pet_record[6]
+        self.experience = pet_record[7]
+
+def do_battle(pet1, pet2, category):
 
     result = {
         # Experience points
         category: 1
     }
 
-    if pet1[category] == pet2[category]:
+    attr1 = pet1.attributes[category]
+    attr2 = pet2.attributes[category]
+
+    if attr1 > attr2:
+        result["victor"] = pet1.name
+    elif attr2 > attr1:
+        result["victor"] = pet2.name
+    else:
         # TODO
         pass
-    elif pet1[category] > pet2[category]:
-        result["victor"] = pet1["name"]
-    else:
-        result["victor"] = pet2["name"]
 
     return result
 
