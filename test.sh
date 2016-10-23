e0b9774e236fd8cfa81c994df35c01943e3cd9f1
Michael Gagnon <mikegagnon@gmail.com>
2016-10-23 15:29:47 -0600

    Beginning of test.sh

---
+++ test.sh
@@ -0,0 +1,18 @@
+#/usr/bin/env bash
+#
+# To setup for the test exectue the following commands first:
+#
+#   $ redis-server
+#   $ rq worker
+#   $ python management.py
+#   $ python arena.py
+#
+
+rm database.db
+python db.py
+
+./new_pet.py --name Abra       --strength 0.2 --agility 0.2 --wit 0.3 --senses 0.3
+./new_pet.py --name Blissey    --strength 0.1 --agility 0.2 --wit 0.4 --senses 0.3
+./new_pet.py --name Charmander --strength 0.5 --agility 0.0 --wit 0.3 --senses 0.2
+./new_pet.py --name Dwebble    --strength 0.3 --agility 0.2 --wit 0.2 --senses 0.3
+./new_pet.py --name Flygon     --strength 0.6 --agility 0.1 --wit 0.1 --senses 0.2
