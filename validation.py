c1f6ad7b23c93693b3bedf36592715dfd725c7e5
Michael Gagnon <mikegagnon@gmail.com>
2016-10-22 21:22:11 -0600

    TODO note in validation.py

--- validation.py
+++ validation.py
@@ -3,6 +3,7 @@ import jsonschema
 
 import error
 
+# TODO: move lask.request.get_json(silent = True) into caller, not here
 def validate_json(schema_name, schema):
 
     # Setting silent=True causes get_json to return None on error, instead
